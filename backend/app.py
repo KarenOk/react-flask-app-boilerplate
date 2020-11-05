8ef0c2fad92c2bb07705d4d929226ae41a8f7399
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-11-05 12:05:14 +0100

    Include additional app set up

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,33 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+from models import db, setup_db
+
+
 def create_app():
     app = Flask(__name__)
+    # setup_db(app)
     CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        """ 
+            Set access control headers
+        """
+
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type, Authorization")
+        response.headers.add("Access-Control-Allow-Methods",
+                             "GET, POST, PATCH, DELETE, OPTIONS")
+
+        return response
 
     @app.route("/")
     def index():
+        """
+            Health Check
+        """
+
         return "This API is up and running"
 
     @app.errorhandler(400)
@@ -42,6 +63,7 @@ def create_app():
         }), 500
 
     return app
+
 
 app = create_app()
 
